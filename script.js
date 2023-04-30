c0e3e59123056c49bcc69f23e3fa4d1d2e47d4c3
r-0n <ronywajah911@gmail.com>
2023-04-30 23:28:27 +0400

    update for javascript

--- script.js
+++ script.js
@@ -2,22 +2,46 @@ window.onload = function() {
     var videosContainer = document.getElementsByClassName("videos")[0];
     var menu = document.getElementsByClassName("menu")[0];
     var menuOptions = menu.getElementsByClassName("options")[0];
+    var button = document.getElementById("btn");
+
+    // Hide the button initially
+    button.style.display = "none";
+
+    // Wait 5 seconds, then show the button
+    setTimeout(function() {
+        button.style.display = "block";
+    }, 35000);
 
     var playButton = document.getElementsByClassName("play")[0];
     playButton.onclick = function() {
         playButton.style.display = "none";
         playInitialMenu();
-    }
+    };
 
-    var catPersonLink = document.getElementById("cat-person");
+    var startButton = document.getElementsByClassName("start")[0];
+    startButton.onclick = function() {
+        showVideos(0, videosContainer.getElementsByClassName("cat"));
+    };
+
+    var catPersonLink = document.getElementById("right-thing");
     catPersonLink.onclick = function() {
         showVideos(0, videosContainer.getElementsByClassName("cat"));
-    }
+    };
 
-    var dogPersonLink = document.getElementById("dog-person");
+    var dogPersonLink = document.getElementById("wrong-thing");
     dogPersonLink.onclick = function() {
         showVideos(0, videosContainer.getElementsByClassName("dog"));
-    }
+    };
+
+    function playInitialMenu() {
+        playVideo(videosContainer.getElementsByClassName("initial")[0], loop = true);
+
+        setTimeout(function() {
+            menu.style.display = "block";
+            menu.getElementsByClassName("initial")[0].style.display = "block";
+            menuOptions.style.display = "block";
+        }, 1000);
+    };
 
     function playInitialMenu() {
         playVideo(videosContainer.getElementsByClassName("initial")[0], loop = true);
@@ -33,6 +57,7 @@ window.onload = function() {
         playVideo(videosContainer.getElementsByClassName("closing")[0], loop = true);
 
         menu.getElementsByClassName("initial")[0].style.display = "none";
+        menuOptions.style.display = "none";
 
         setTimeout(function() {
             menu.style.display = "block";
@@ -59,32 +84,43 @@ window.onload = function() {
     }
 
     function showVideos(index, videos) {
-        if (index < (videos.length - 1)) {
-            hasNextVideo = true;
+        if (index < videos.length) {
+            menu.style.display = "none";
+
+            var video = playVideo(videos[index]);
+
+            video.addEventListener("ended", function() {
+                var options = videoContainer.getElementsByClassName("options")[0];
+
+                if (options) {
+                    var optionButtons = options.getElementsByTagName("button");
+
+                    // Hide all options initially
+                    options.style.display = "none";
+                    for (var i = 0; i < optionButtons.length; i++) {
+                        optionButtons[i].style.display = "none";
+                    }
+
+                    // Show options for the current video and attach click event handlers
+                    options.style.display = "block";
+                    for (var i = 0; i < optionButtons.length; i++) {
+                        var nextIndex = parseInt(optionButtons[i].getAttribute("data-next-index"));
+                        if (nextIndex === index + 1) {
+                            optionButtons[i].style.display = "block";
+                            optionButtons[i].onclick = function() {
+                                options.style.display = "none";
+                                showVideos(nextIndex, videos);
+                            };
+                        }
+                    }
+
+                } else {
+                    showVideos(index + 1, videos);
+                }
+            });
         } else {
-            hasNextVideo = false;
-        }
-
-        menu.style.display = "none";
-
-        var video = playVideo(videos[index]);
-
-        video.addEventListener("timeupdate", function() {
-            var currentTime = (this.currentTime / this.duration) * 100;
-
-            if (hasNextVideo && currentTime > 70) {
-                nextVideo = videos[index + 1];
-                nextVideoTag = nextVideo.getElementsByTagName("video")[0];
-                nextVideoTag.preload = "auto";
-            }
-        })
-
-        video.onended = function() {
-            if (hasNextVideo) {
-                showVideos(index + 1, videos);
-            } else {
-                playClosingMenu();
-            }
+            playClosingMenu();
         }
     }
-}
+
+};
